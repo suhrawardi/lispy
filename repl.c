642375f6d5b88448922821e69dce2055a2217a5a
jarra <suhrawardi@gmail.com>
2018-03-02 12:27:37 +0100

    Improved the REPL

--- repl.c
+++ repl.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
 
 int main(int argc, char** argv) {
 
@@ -8,11 +9,13 @@ int main(int argc, char** argv) {
   puts("Press Ctrl+c to Exit");
 
   while(1) {
-    fputs("lispy> ", stdout);
+    char* input = readline("lispy> ");
 
-    fgets(input, 2048, stdin);
+    add_history(input);
 
     printf("No, you're a %s", input);
+
+    free(input);
   }
 
   return 0;
